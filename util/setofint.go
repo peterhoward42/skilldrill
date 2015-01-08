c0bd8c84f614eb1759cd6595ca4b945dbbbc3aaf
Peter Howard <peterhoward42@gmail.com>
2015-01-08 23:38:37 +0000

    commit before global edit of int32 to int

--- util/setofint.go
+++ util/setofint.go
@@ -10,19 +10,40 @@ func NewSetOfInt() *SetOfInt {
 	return &SetOfInt{data: map[int32]bool{}}
 }
 
+// The function Overwrite() replaces the set's content with that of the
+// given slice.
+func (s *SetOfInt) Overwrite(newContent []int32) {
+	s.data = map[int32]bool{}
+	for _, value := range newContent {
+		s.Add(value)
+	}
+}
+
+// The function Add(), adds the given value into the set.
+func (s *SetOfInt) Add(val int32) {
+	s.data[val] = true
+}
+
+// The function Contains() tests for the presence of the given value in
+// the set.
 func (s *SetOfInt) Contains(val int32) bool {
 	_, ok := s.data[val]
 	return ok
-}
-
-func (s *SetOfInt) Add(val int32) {
-	s.data[val] = true
 }
 
 func (s *SetOfInt) MarshalYAML() (interface{}, error) {
 	return s.AsSlice(), nil
 }
 
+func (s *SetOfInt) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	tmpSlice := make([]int32, 0)
+	err := unmarshal(&tmpSlice)
+	s.Overwrite(tmpSlice)
+	return err
+}
+
+// The function AsSlice() provides a slice of integers comprising the
+// members of the set.
 func (set *SetOfInt) AsSlice() (slice []int32) {
 	for k, _ := range set.data {
 		slice = append(slice, k)
--- util/setofint_test.go
+++ util/setofint_test.go
@@ -6,11 +6,14 @@ import (
 
 func TestSetOfInt(t *testing.T) {
 	set := NewSetOfInt()
+
+    // Do some adding, including duplicate addition
 	set.Add(1)
 	set.Add(2)
 	set.Add(3) // note is a duplicate add
 	set.Add(3)
 
+    // Check count and content
 	count := len(set.data)
 	if count != 3 {
 		t.Errorf("Count wrong %d, expected %d", count, 3)
@@ -21,9 +24,18 @@ func TestSetOfInt(t *testing.T) {
 	if set.Contains(9999) {
 		t.Errorf("Should say does not contain 9999")
 	}
+
+    // Check conversion to slice
 	asSlice := set.AsSlice()
 	count = len(asSlice)
 	if count != 3 {
 		t.Errorf("Slice length wrong: %d, expected 3.", count)
 	}
+
+    // Check overwrite from slice
+	set = NewSetOfInt()
+	set.Add(99)
+	set.Overwrite([]int32{1, 2, 3})
+	AssertEqInt32(t, len(set.data).(int32), 3, "length of set")
+	AssertTrue(t, set.Contains(1), "set contents")
 }
